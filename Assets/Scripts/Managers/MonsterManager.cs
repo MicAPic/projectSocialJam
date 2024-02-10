d9c5a1ed8cc85a0099ba8f7200ab4e7b9ea87680
Michael Pavlov <micha.pavlov@gmail.com>
2024-02-10 23:55:30 +0300

    Integrate two more BGs

--- Assets/Scripts/Managers/MonsterManager.cs
+++ Assets/Scripts/Managers/MonsterManager.cs
@@ -1,11 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using Rooms;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class MonsterManager : MonoBehaviour
 {
